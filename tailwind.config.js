243827c4b2bc1f526069e7febd0c72005220e476
roman.rolgin <irolgin@gmail.com>
2024-10-21 21:31:40 +0200

    make it fail

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-// import {
-//   isolateInsideOfContainer,
-//   scopedPreflightStyles,
-// } from "tailwindcss-scoped-preflight";
+import {
+  isolateInsideOfContainer,
+  scopedPreflightStyles,
+} from "tailwindcss-scoped-preflight";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -10,8 +10,8 @@ export default {
     extend: {},
   },
   plugins: [
-    // scopedPreflightStyles({
-    //   isolationStrategy: isolateInsideOfContainer("some-prefix"),
-    // }),
+    scopedPreflightStyles({
+      isolationStrategy: isolateInsideOfContainer("some-prefix"),
+    }),
   ],
 };
